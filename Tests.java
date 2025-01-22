881c730223bdc73ea739b1ae8dbb3bd7d26d11cb
andyhudak1126 <andyhudak1126@gmail.com>
2025-01-21 20:37:34 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,21 @@
+//Andrew Hudak
+//Module 2 Assignment 1
+//Computer Science 1302 - online
+//1/21/2024
+
+public class Tests {
+    public static void main(String[] args)
+    {
+        //Here is all of our string inputs for 3 different test scores
+        System.out.println("Test score 1: 88.2");
+        System.out.println("Test score 2: 87.4");
+        System.out.println("Test score 3: 92.6");
+
+        //calculated average of all 3 test scores
+        System.out.println("The average of 3 test scores is: 89.4");
+    }
+
+
+
+
+}
